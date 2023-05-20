de055b682b36343398947710d6106f4bcbeeb09f
Kennedy Musau <kennedy.musauu@gmail.com>
2023-05-20 13:59:34 +0300

    completed assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  function onChangeHandler(e) {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("I submit");
+    // console.log("I submit");
+    console.log(formData.username, formData.password);
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="username" placeholder="Enter username..." />
-      <input type="password" name="password" placeholder="Enter password..." />
+      <input
+        type="text"
+        name="username"
+        placeholder="Enter username..."
+        onChange={onChangeHandler}
+        value={formData.username}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter password..."
+        onChange={onChangeHandler}
+        value={formData.password}
+      />
       <button>Login</button>
     </form>
   );
